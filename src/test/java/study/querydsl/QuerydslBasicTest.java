f788cb96737eefc4e604c91ce0fb56f645a031ff
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-09 22:33:29 +0900

    subquery

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -20,6 +21,7 @@ import study.querydsl.entity.Team;
 
 import java.util.List;
 
+import static com.querydsl.jpa.JPAExpressions.*;
 import static org.assertj.core.api.Assertions.*;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
@@ -54,20 +56,21 @@ public class QuerydslBasicTest {
     @Test
     public void startJPQL(){
         //member1을 찾아라.
-        String qlString = "select m from Member m where m.username = :username";
-
-        Member findOne = em.createQuery(qlString, Member.class)
+        Member findByJpql =
+                em.createQuery("select m from Member m where m.username = :username", Member.class)
                 .setParameter("username", "member1")
                 .getSingleResult();
 
-        assertThat(findOne.getUsername()).isEqualTo("member1");
+        assertThat(findByJpql.getUsername()).isEqualTo("member1");
     }
     @Test
     public void startQuerydsl(){
+        QMember m = new QMember("m");
+
         Member findMember = queryFactory
-                .select(member)
-                .from(member)
-                .where(member.username.eq("member1"))//파라미터 바인딩 처리
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
@@ -84,6 +87,7 @@ public class QuerydslBasicTest {
     }
     @Test
     public void searchAndParam(){ //and 조건인 경우에 , 로 이어서 쭉 쓸 수 있다
+
         Member findMember = queryFactory
                 .selectFrom(member)
                 .where(member.username.eq("member1")
@@ -96,28 +100,31 @@ public class QuerydslBasicTest {
     @Test
     public void resultFetch(){
 
+//        //리스트 조회
 //        List<Member> fetch = queryFactory
 //                .selectFrom(member)
 //                .fetch();
 //
+//        //단건 조회
 //        Member fetchOne = queryFactory
 //                .selectFrom(member)
 //                .fetchOne();
 //
+//        //첫 데이터만 조회
 //        Member fetchFirst = queryFactory
 //                .selectFrom(member)
 //                .fetchFirst();
-
+//
 //        QueryResults<Member> results = queryFactory
 //                .selectFrom(member)
 //                .fetchResults();
-//        results.getTotal();
-//        List<Member> content = results.getResults();
+
+        //results.getTotal();
+        //List<Member> content = results.getResults();
 
         long total = queryFactory
                 .selectFrom(member)
                 .fetchCount();
-
     }
     /*
 
@@ -137,6 +144,7 @@ public class QuerydslBasicTest {
                 .where(member.age.eq(100))
                 .orderBy(member.age.desc(), member.username.asc().nullsLast())
                 .fetch();
+
         Member member5 = result.get(0);
         Member member6 = result.get(1);
         Member memberNull = result.get(2);
@@ -168,7 +176,8 @@ public class QuerydslBasicTest {
                 .orderBy(member.username.desc())
                 .offset(1) //하나 스킵하고
                 .limit(2) //두 개 가져옴
-                .fetchResults();
+                .fetchResults(); //카운트 쿼리 하나 나가고 + 컨텐츠 용 쿼리 하나 나감
+
 
         assertThat(queryResults.getTotal()).isEqualTo(4);
         assertThat(queryResults.getLimit()).isEqualTo(2);
@@ -177,10 +186,12 @@ public class QuerydslBasicTest {
     }
     @Test
     public void aggregation(){
+
+
         List<Tuple> result = queryFactory
                 .select(
                         member.count(),
-                        member.age.sum(),
+                        member.age.sum().as("총합"),
                         member.age.avg(),
                         member.age.max(),
                         member.age.min()
@@ -200,6 +211,7 @@ public class QuerydslBasicTest {
      */
     @Test
     public void group(){
+
         List<Tuple> result = queryFactory
                 .select(team.name, member.age.avg())
                 .from(member)
@@ -222,7 +234,7 @@ public class QuerydslBasicTest {
     public void join(){
         List<Member> result = queryFactory
                 .selectFrom(member)
-                .join(member.team, team)
+                .leftJoin(member.team, team)
                 .where(team.name.eq("teamA"))
                 .fetch();
 
@@ -231,6 +243,7 @@ public class QuerydslBasicTest {
                 .containsExactly("member1", "member2");
     }
     /*
+    세타 조인
     회원의 이름이 팀 이름과 같은 회원 조회
      */
     @Test
@@ -243,7 +256,6 @@ public class QuerydslBasicTest {
                 .from(member, team)
                 .where(member.username.eq(team.name))
                 .fetch();
-
 
         assertThat(result)
                 .extracting("username")
@@ -278,7 +290,7 @@ public class QuerydslBasicTest {
         List<Tuple> result = queryFactory
                 .select(member, team)
                 .from(member)
-                .leftJoin(team).on(member.username.eq(team.name))
+                .leftJoin(team).on(member.username.eq(team.name)) //보통은 leftJoin(member.team, team)이렇게 해야 하는데 바로 team을 넣기 때문에 on 절에 따른 조건을 적용했을 때 존재하는 경우만 team 데이터를 끌고온다
                 .fetch();
 
         for (Tuple tuple : result) {
@@ -320,6 +332,100 @@ public class QuerydslBasicTest {
 
     }
 
+    //서브쿼리
+    // 나이가 가장 많은 회원 조회
+    @Test
+    public void subQuery(){
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(
+                        select(memberSub.age.max())
+                                .from(memberSub)
+                ))
+                .fetch();
+
+        assertThat(result).extracting("age")
+                .containsExactly(40);
+    }
+
+
+    //나이가 평균 이상인 회원 조회
+    @Test
+    public void subQueryGoe(){
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.goe(
+                        select(memberSub.age.avg())
+                                .from(memberSub)
+                ))
+                .fetch();
+
+        assertThat(result).extracting("age")
+                .containsExactly(30, 40);
+    }
+
+    //멤버의 나이가 10살 초과인 회원 조회 (억지성 예제지만 in사용위함)
+    @Test
+    public void subQueryIn(){
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.in(
+                        select(memberSub.age)
+                                .from(memberSub)
+                                .where(memberSub.age.gt(10)) //10살 초과
+                ))
+                .fetch();
+
+        assertThat(result).extracting("age")
+                .containsExactly(20, 30, 40);
+    }
+
+
+    //select 절에 서브쿼리 사용 예시
+    @Test
+    public void selectSubQuery(){
+        QMember memberSub = new QMember("memberSub");
+
+        List<Tuple> result = queryFactory
+                .select(member.username,
+                        select(memberSub.age.avg())
+                                .from(memberSub))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+
+    }
+    /*
+    * from 절의 서브쿼리 한계
+    jpql 서브쿼리의 한계점으로 fmro 절의 서브쿼리는 지원하지 않는다
+    * querydsl도 jpql 빌더 역할이므로 당연히 from 절의 서브쿼리 지원 안됨
+    * */
+
+    /*
+    *from 절의 서브쿼리 해결 방안
+    * 1. 서브쿼리를 join으로 변경 -> 대부분의 경우 가능하겠지만 불가능한 상황도 있음
+    * 2. 애플리케이션에서 쿼리를 2번 분리해서 실행한다
+    * 3. 정 안되면 nativeSQL 사용한다
+    *
+    * 무엇보다 정말 from 절에서 서브쿼리가 필요한지에 대해서는 고민해야 한다
+    * 화면 단에서 렌더링 하는 여러 데이터들을 구성하는 것은 프레젠테이션 계층에서 수행하고
+    * db는 기본적으로 데이터를 퍼올리는 역할이라는 것을 인지하자...
+    * 물론 쿼리에서 적절한 조건절, 필터링을 수행하는 것은 너무 중요하지만
+    * 한 방쿼리로 많은걸 해결하려기보다 적절하게 쿼리를 쪼개는 것도 방안이 될 수 있음을 인지하는게 필요
+    *
+    * */
 
 
 
